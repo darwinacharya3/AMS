b9116bde89b4007700589701248c10924cfc80c3
darwinacharya3 <acharyadarwin3@gmail.com>
2025-02-12 12:27:13 +0545

    AMS

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'screens/enrollment_screen.dart';
+
+void main() {
+  runApp(const EMSApp());
+}
+
+class EMSApp extends StatelessWidget {
+  const EMSApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'EMS Enrollment',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const EnrollmentScreen(),
+    );
+  }
+}
+
+
+
+
+
+
+
